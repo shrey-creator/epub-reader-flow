07bfe9c07c7fcb974e6e26372adc9c556ab6d902
Shrey Agrawal <shrey.agrawal003@gmail.com>
2023-10-27 22:16:19 +0530

    added log

--- app/epub-reader.tsx
+++ app/epub-reader.tsx
@@ -9,6 +9,7 @@ const EpubReader = (params:any) => {
 
       const IFRAME_URL=process.env.NEXT_PUBLIC_IFRAME_URL
       const IFRAME_URL_WITH_QUERY=`${IFRAME_URL}/?url=${params.url}`
+      console.log('IFRAME_URL_WITH_QUERY',IFRAME_URL_WITH_QUERY)
 
   return (
     <iframe
